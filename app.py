a56192caaf1ca2a90e49c41c052df042ebcb52c1
Fahad Zahid <105378742+fahad0071@users.noreply.github.com>
2026-03-14 23:19:07 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, request, jsonify
+import os
+import threading
+from flask import Flask
+# from pyngrok import ngrok, conf
+import os
+import logging
+import sys
+import torch
+from llama_index.core import PromptTemplate
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Settings
+from llama_index.core import Settings
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex
+from IPython.display import Markdown, display
+from llama_index.core import VectorStoreIndex
+from dotenv import load_dotenv
+import os
+from llama_index.llms.groq import Groq
+
+load_dotenv()
+os.system(f'huggingface-cli login --token {os.getenv("HUGGINGFACE_TOKEN")}')
+
+# print("Enter your authtoken, which can be copied from https://dashboard.ngrok.com/get-started/your-authtoken")
+# conf.get_default().auth_token = os.getenv('NGROK_AUTH')
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Open a ngrok tunnel to the HTTP server
+# public_url = ngrok.connect(5000).public_url
+
+# Update any base URLs to use the public ngrok URL
+# app.config["BASE_URL"] = public_url
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+
+LLAMA2_7B_CHAT = "meta-llama/Llama-2-7b-chat-hf"
+
+
+selected_model = LLAMA2_7B_CHAT
+
+SYSTEM_PROMPT = """You are an AI assistant that answers questions in a friendly manner, based on the given source documents. Here are some rules you always follow:
+- Generate human readable output, avoid creating output with gibberish text.
+- Generate only the requested output, don't include any other language before or after the requested output.
+- Never say thank you, that you are happy to help, that you are an AI agent, etc. Just answer directly.
+- Generate professional language typically used in business documents in North America.
+- Never generate offensive or foul language.
+"""
+
+query_wrapper_prompt = PromptTemplate(
+    "[INST]<<SYS>>\n" + SYSTEM_PROMPT + "<</SYS>>\n\n{query_str}[/INST] "
+)
+
+llm = Groq(model="llama3-70b-8192", api_key=os.getenv('GROQ_API_KEY'))
+
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+Settings.llm = llm
+Settings.embed_model = embed_model
+
+
+
+
+documents =SimpleDirectoryReader(input_files=["parsedData.csv"]).load_data()
+
+
+index = VectorStoreIndex.from_documents(documents)
+# set Logging to DEBUG for more detailed outputs
+query_engine = index.as_query_engine()
+# print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}/\"".format(public_url, 5000))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/')
+def hello_world():
+    return render_template('chat.html')
+
+@app.route('/get', methods=['GET','POST'])
+def chat():
+     user_input = request.form["msg"]
+     return str(query_engine.query(user_input))
+# Run the Flask application
+if __name__ == '__main__':
+    threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
